e539d2d37c03d74dc32c6549e9d83836f69c32e8
ShurinO <arestarx@ukr.net>
2018-06-25 09:56:56 +0300

    Initial commit

---
+++ src/com/brainacad/oop/testenum2/Main.java
@@ -0,0 +1,15 @@
+package com.brainacad.oop.testenum2;
+
+public class Main {
+    public static void main(String[] args) {
+        Card[] cards = new Card[Suit.values().length*Rank.values().length];
+        for (int i = 0; i < Suit.values().length; i++) {
+            for (int j = 0; j < Rank.values().length; j++) {
+                cards[i*Rank.values().length+j] = new Card(Suit.values()[i],Rank.values()[j]);
+            }
+        }
+        for (Card card : cards) {
+            System.out.println(card);
+        }
+    }
+}
